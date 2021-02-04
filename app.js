c80ac126fd9962500e9751e483d78ced483bd491
nshimiyimana <njeanluc828@gmail.com>
2021-02-04 23:05:37 +0200

    we end to readour filewith using function approach

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
 const fs=require("fs");
 
 const fileName="target.txt";
+
+
+
+
+//APPLY FUNCTIONAL APPROACH IN THIS operation to read file
+
+//for error handler
+
+const errorHandler=(err)=>{
+    console.log(err);
+}
+
+
+//for data handler
+
+const dataHandler=(data)=>{
+    console.log(data.toString());
+}
+
+
 fs.readFile(fileName,(err,data)=>{
-    if(err){
-        throw new Error('unable to read this file')
-    }
+    if(err) errorHandler(err)
     else{
-        console.log(data.toString());  //by default this data is buffer as many numbers we need to change into string by using toString()
+        console.log(dataHandler(data)) ; //by default this data is buffer as many numbers we need to change into string by using toString()
     }
 })
 
 console.log('asynchroneous programming... this text displayed before above because we use async');
  
+
+
